dd6b098bc10a273293aee71da91ce553653377ec
kkatayama <kkatayama@udel.edu>
2023-02-20 20:18:41 -0500

    added configs

--- public/config.js
+++ public/config.js
@@ -3,16 +3,12 @@ window.playername = "IPTVEditor Web Player";
 
 /*----- DNS -----*/
 //Iptv provider dns url (for example "http://domain.com:80")
-window.dns = "http://domain.com:80";
+window.dns = "http://line.lemotv.cc";
 
 /*----- CORS -----*/
-/*/ False if iptv provider has the Access-Control-Allow-Origin set on "*" or allows your player domain.
-It will work with most of the providers.
-
-Change in "true" in the other case. Player will use "proxy.php" file to manage the requests.  
-If true, change also the $cors value in proxy.php
-*/
-window.cors = false; //true
+/*/ Change on false if iptv provider has the Access-Control-Allow-Origin set on "*" or allows your player domain.
+Leave on "true" in the other case. Player will use "proxy.php" file to manage the requests.  */
+window.cors = false; //false
 
 /*---- HTTPS -----*/
 /* If streams are using an ssl protol change this box on true.*/
@@ -21,9 +17,7 @@ window.https = false;  //true;
 /*----- TMDB API [OPTIONAL] -----*/
 /* By default player will use movie info from the provider. In case these info are missing it will be used tmdb as alternative
 Here you can get a tmdb api key: https://developers.themoviedb.org/3/getting-started/introduction  */
-window.tmdb = "";
-
-
+window.tmdb = "845ca07bd9b746fd92ad14a36f4d52aa";
 
 
 
