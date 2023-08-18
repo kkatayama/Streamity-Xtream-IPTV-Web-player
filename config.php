b9d099e13cb9e328cc561deb961097d2046a5aa8
kkatayama <kkatayama@udel.edu>
2023-08-17 22:47:44 -0400

    added custom config scripts and files

--- config.php
+++
@@ -1,9 +0,0 @@
-<?php
-$db_url = "localhost";
-$db_name = "lemo2"; //database name
-$db_username = "mangoboat"; //database account username
-$db_password = "smokekush2"; //database account password
-$epg_url = "http://line.lemotv.cc/epg.php?username=3S6ySq44tg&password=6345364334434"
-$epg_valid_hours = "+12 hours";
-
-?>
--- public/config.php
+++ public/config.php
@@ -5,5 +5,5 @@ $db_username = "mangoboat"; //database account username
 $db_password = "smokekush2"; //database account password
 $epg_url = "http://line.lemotv.cc/epg.php?username=3S6ySq44tg&password=6345364334434"
 $epg_valid_hours = "+12 hours";
-
+$cors = false;
 ?>
